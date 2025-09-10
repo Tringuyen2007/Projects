068059a7fddee039fa3dca9d050ef4156ae34b6a
Tri Nguyen <135263211+Tringuyen2007@users.noreply.github.com>
2025-09-10 12:50:26 -0500

    Add files via upload

---
+++ Fighter.py
@@ -0,0 +1,63 @@
+
+import random
+
+def fight():
+    # List of fighters
+    fighters = ["Rodtang", "Jon Jones", "Du Bronx", "Conor Mcgregor", "Khalib Nurmagomedov", "Khamzat Chimaev", "Alex Pierera", "Dimitrius Johnson", "Mike Tyson", "Muhamad Ali"] #list of fighters”
+
+
+    print("Welcome to the octagon!")
+
+    # Display the list of fighters
+    print("Here are the list of fighters:")
+    for abc, fighter in enumerate(fighters, start=1):
+        print(f"{abc}. {fighter}")
+
+    # Let the user pick a fighter
+    while True:
+    
+        choice = (input("Enter the name of the fighter you would like to represent you: "))
+        if choice in fighters:
+            
+            break
+        else:
+            print("Invalid fighter choice; please choose a fighter from the list.")
+    else:
+        print("Please enter a valid fighter")
+        
+       
+
+    print(f"You chose {choice}")
+
+    # Simulate matchups
+    print("Here are your matchups:")
+    wins = 0
+    losses = 0
+
+    for fighter in fighters:
+        if fighter == choice:
+            continue
+
+        print(f"{choice} vs {fighter}...", end=" ")
+        result = random.choice(["win", "loss"])
+        if result == "win":
+            print("You won!")
+            wins += 1
+        else:
+            print("You lost!")
+            losses += 1
+
+    # Display results
+    print(f"\nResults for {choice}:")
+    print(f"Wins: {wins}")
+    print(f"Losses: {losses}")
+
+    if wins > losses:
+        print(f"Your fighter {choice} record is {wins} wins and {losses} losses, congratulations your fighter is the winner overall!")
+    else:
+        print(f"Your fighter{choice} record is {wins} wins and {losses} losses, better luck next time!")
+
+if __name__ == "__main__":
+    fight()
+
+
